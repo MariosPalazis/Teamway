87a57fb29adc69aaa5844cb5c1f724a3a003b8c9
Marios <marios.palazis@iccs.gr>
2023-04-24 14:51:19 +0300

    frontend v3 results page pending

--- frontend/src/Components/SingleQuestion.tsx
+++ frontend/src/Components/SingleQuestion.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction ,useEffect,useState} from 'react'
+import { QuestionsType } from '../interfaces/Questions';
 
-function SingleQuestion() {
+
+interface SingleQuestionProps {
+  questionsLength: number;
+  questionData: QuestionsType;
+  setKeepPoints: Dispatch<SetStateAction<number>>;
+  setContinueButton: Dispatch<SetStateAction<boolean>>;
+  questionNumber: number;
+}
+function SingleQuestion(props: SingleQuestionProps) {
+  
+
+  const[selected, setSelected] = useState<number>(-1)
+
+  useEffect(()=>{
+    setSelected(-1)
+  },[props.questionNumber])
+
+  const clickAnswer = async (points:number, selected:number) => {
+    props.setKeepPoints(points);
+    setSelected(selected);
+    props.setContinueButton(true);
+  }
+
   return (
-    <div>Question</div>
+    <div className='questionPanel'>
+      <h3>Question {props.questionNumber+1}/{ props.questionsLength}</h3>
+      <div className='questionBox'>
+        <div className='question'>{props.questionData.question}</div>
+        <div className='answers'>
+          {
+            props.questionData?.answers.map((item, key)=>{
+              if(selected === key){
+                return <div key={key} className='singleAnswer selected' onClick={() => clickAnswer(item.points, key)} >{item.text}</div>
+              }else{
+                return <div key={key} className='singleAnswer' onClick={() => clickAnswer(item.points, key)} >{item.text}</div>
+              }
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/Questions.tsx
+++ frontend/src/Pages/Questions.tsx
@@ -3,12 +3,15 @@ import axios from 'axios';
 import data from './data.json';
 import SingleQuestion from '../Components/SingleQuestion';
 import { QuestionsType } from '../interfaces/Questions';
+import Button from '@mui/material/Button';
 
 function Questions() {
 
-  const[questions, setQuestions] = useState<QuestionsType[]>();
-  const[points, setPoints] = useState<number>();
+  const[questions, setQuestions] = useState<QuestionsType[]>([]);
+  const[points, setPoints] = useState<number>(0);
   const[questionNumber, setQuestionNumber] = useState<number>(0);
+  const[continueButton, setContinueButton] = useState<boolean>(false);
+  const[keeppoints, setKeepPoints] = useState<number>(-1);
 
   useEffect(()=>{
     const loadData = async() =>{
@@ -19,13 +22,26 @@ function Questions() {
     loadData();
   },[])
 
+  const nextClick = async() =>{
+    setPoints(points+keeppoints);
+    setContinueButton(false);
+    setQuestionNumber(questionNumber+1)
+  }
+  if(questions.length===0){
+    return <></>;
+  }
  
+  console.log(questionNumber, questions.length)
   return (
     <div className='App'>
       <div className='questionsPanel'>
         <h2>Answer all the questions to get a result!</h2>
-        <SingleQuestion questionsLength={questions?.length} questionData={questions[questionNumber]} setPoints={setPoints} />
-        
+        {questions.length>0 && <SingleQuestion questionsLength={questions?.length} questionNumber={questionNumber} questionData={questions[questionNumber]} setContinueButton={setContinueButton}  setKeepPoints={setKeepPoints}/>}
+        {
+          continueButton
+          ?<Button variant="outlined" color="success" className='questionButton' onClick={nextClick} >{questionNumber+1===questions.length?"See Results":"Next Question"}</Button>
+          :<Button variant="outlined" className='questionButton' disabled>{questionNumber+1===questions.length?"See Results":"Next Question"}</Button>
+        }
       </div>
     </div>
    
